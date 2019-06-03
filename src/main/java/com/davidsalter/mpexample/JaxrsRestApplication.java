a34ca324c1fd8796fdda2ea083fdb4debb6bb38a
David Salter <wedevelopinjava@gmail.com>
2019-06-03 16:24:50 +0100

    Initial Version

---
+++ src/main/java/com/davidsalter/mpexample/JaxrsRestApplication.java
@@ -0,0 +1,11 @@
+package com.davidsalter.mpexample;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ *
+ */
+@ApplicationPath("/data")
+public class JaxrsRestApplication extends Application {
+}
